5d595bcf7030aa8a7c1fe0afdcb35c8f30c8470f
EddyEscalanteU <eescalante@ucb.edu.bo>
2024-01-09 21:04:14 -0400

    ORM Example

---
+++ DOCENTE/ORM/WebAppWithORM/WebAppWithORM/Controllers/CompaniesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppWithORM.Interface;
+
+namespace WebAppWithORM.Controllers
+{
+    [Route("api/companies")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly ICompanyRepository _companyRepo;
+
+        public CompaniesController(ICompanyRepository companyRepo)
+        {
+            _companyRepo = companyRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCompanies()
+        {
+            try
+            {
+                var companies = await _companyRepo.GetCompanies();
+                return Ok(companies);
+            }
+            catch (Exception ex)
+            {
+                //log error
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
