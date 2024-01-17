fa10cdacdb8b6819c7bcd6f7dca8864eb4dd65df
Seegioberserk <cortezdiazjosealfredo@gmail.com>
2024-01-17 19:27:14 -0400

    add

--- Seegioberserk/primer_parcial/AppY/README.md
+++ Seegioberserk/primer_parcial/AppY/README.md
@@ -1,4 +1,4 @@
-# App1
+# AppY
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.10.
 
--- Seegioberserk/primer_parcial/AppY/src/app/app.component.spec.ts
+++ Seegioberserk/primer_parcial/AppY/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'App1' title`, () => {
+  it(`should have the 'AppY' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('App1');
+    expect(app.title).toEqual('AppY');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, App1');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AppY');
   });
 });
