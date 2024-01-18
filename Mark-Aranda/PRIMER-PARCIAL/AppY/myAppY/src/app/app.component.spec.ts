d495f0f90f2000755ff7dda09e121506ac831ed1
Mark-Aranda <155692745+Mark-Aranda@users.noreply.github.com>
2024-01-17 20:25:53 -0400

    primer parcial

--- Mark-Aranda/PRIMER-PARCIAL/AppY/myAppY/src/app/app.component.spec.ts
+++ Mark-Aranda/PRIMER-PARCIAL/AppY/myAppY/src/app/app.component.spec.ts
@@ -7,22 +7,23 @@ describe('AppComponent', () => {
       imports: [AppComponent],
     }).compileComponents();
   });
+
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'myApp1' title`, () => {
+  it(`should have the 'myAppY' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myApp1');
+    expect(app.title).toEqual('myAppY');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, myApp1');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, myAppY');
   });
 });
