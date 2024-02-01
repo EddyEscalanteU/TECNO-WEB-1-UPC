619646bbc352b731bfecd4aed9a2e48038a8ac0b
Jeffrey Valdivia <jeffreyvaldiviaflores@gmail.com>
2024-01-31 21:05:42 -0400

    TERCER_PARCIAL

--- Jeffreyvf/TERCER_PARCIAL/angular-monorepo/apps/AppC/src/app/app.config.ts
+++ Jeffreyvf/TERCER_PARCIAL/angular-monorepo/apps/AppC/src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';//NUEVA REFERENCIA
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(),
