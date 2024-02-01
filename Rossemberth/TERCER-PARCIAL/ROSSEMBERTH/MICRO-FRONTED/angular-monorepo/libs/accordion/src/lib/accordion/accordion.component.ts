feddb7a0d6bbad5d2ff1ac617a38159ef3e081d4
desarrollo <eescalante@geoautomotriz.com.bo>
2024-02-01 09:55:58 -0400

    examen final

---
+++ Rossemberth/TERCER-PARCIAL/ROSSEMBERTH/MICRO-FRONTED/angular-monorepo/libs/accordion/src/lib/accordion/accordion.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'angular-monorepo-accordion',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './accordion.component.html',
+  styleUrl: './accordion.component.css',
+})
+export class AccordionComponent {
+  
+}
